b523b87a6cd6c1d119819a6f6c1eb3cb568c52b2
Zahid Ul Islam <zahidpy@gmail.com>
2021-05-01 17:51:44 +0600

    click function added

--- day_1_expanding_cards/script.js
+++ day_1_expanding_cards/script.js
@@ -1,1 +1,14 @@
+const panels = document.querySelectorAll(".panel");
 
+for(const panel of panels){
+    panel.addEventListener("click", function(){
+        removeActive();
+        panel.classList.add("active");
+    })
+}
+
+function removeActive(){
+    for( panel of panels){
+        panel.classList.remove("active");
+    }
+}
