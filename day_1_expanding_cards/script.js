35add1920e41a0f496af4a90dece3aec0268e19d
Zahid Ul Islam <zahidpy@gmail.com>
2021-05-01 17:52:33 +0600

    code organised

--- day_1_expanding_cards/script.js
+++ day_1_expanding_cards/script.js
@@ -1,14 +1,14 @@
 const panels = document.querySelectorAll(".panel");
 
-for(const panel of panels){
-    panel.addEventListener("click", function(){
+for (const panel of panels) {
+    panel.addEventListener("click", function () {
         removeActive();
         panel.classList.add("active");
     })
 }
 
-function removeActive(){
-    for( panel of panels){
+function removeActive() {
+    for (panel of panels) {
         panel.classList.remove("active");
     }
 }
